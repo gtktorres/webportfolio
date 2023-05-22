f578b89f515937c35152efa5e0f1e783e7c922dd
gtktorres <gtktorres@gmail.com>
2023-05-22 16:07:34 -0600

    bp to pkg

--- monsters/index.mjs
+++ monsters/index.mjs
@@ -2,8 +2,8 @@ import * as dotenv from 'dotenv'
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
-import {bp} from 'body-parser';
-const json = bp;
+import {pkg} from 'body-parser';
+const json = pkg;
 import express from 'express';
 import mongoose from 'mongoose';
 import { typeDefs } from './typeDefs.js';
